116ca613169f100b0431d17284df541130b9a1a5
matheusbertoti <100535920+matheusbertoti@users.noreply.github.com>
2022-06-23 00:26:40 -0300

    Criando arquivo com palavras do dicionario

--- cod_anagrama.py
+++ cod_anagrama.py
@@ -1,30 +1,30 @@
-# Codificando letras para numeros (ordem alfabetica)
-a = 1
-b = 2
-c = 3
-d = 4
-e = 5
-f = 6
-g = 7
-h = 8
-i = 9
-j = 10
-k = 11
-l = 12
-m = 13
-n = 14
-o = 15
-p = 16
-q = 17
-r = 18
-s = 19
-t = 20
-u = 21
-v = 22
-w = 23
-x = 24
-y = 25
-z = 26
+# # Codificando letras para numeros (ordem alfabetica)
+# a = 1
+# b = 2
+# c = 3
+# d = 4
+# e = 5
+# f = 6
+# g = 7
+# h = 8
+# i = 9
+# j = 10
+# k = 11
+# l = 12
+# m = 13
+# n = 14
+# o = 15
+# p = 16
+# q = 17
+# r = 18
+# s = 19
+# t = 20
+# u = 21
+# v = 22
+# w = 23
+# x = 24
+# y = 25
+# z = 26
 
 # Importanto as bibliotecas necessarias
 from itertools import permutations
@@ -42,12 +42,19 @@ letras = sorted(palavra)
 # Removendo espaços vazios na lista de letras
 while ' ' in letras:
     letras.remove(' ')
-
-
 print(letras)
+
+# Fazendo leitura do dicionario 
+lista_de_palavras = open('dicionario_pt-br.txt', "r", encoding='utf-8')
+dicionario = []
+for line in lista_de_palavras:
+    dicionario.append(line.strip())
+
 
 # criando permutacoes da palavra digitada
 resultado = []
+resultado_real = []
+
 possibilidades = permutations(letras,tamanho_palavra)
 for i in list(possibilidades):
     if i not in resultado:    
@@ -55,3 +62,11 @@ for i in list(possibilidades):
     else: 
         skip
 print('Total de possibilidades: ' + str(len(resultado)))
+
+for i in resultado:
+    if i in dicionario:
+       resultado_real.append(i)
+    else:
+       skip
+    resultado_real.append(i)
+print('Total de possibilidades reais: ' + str(len(resultado_real)))
