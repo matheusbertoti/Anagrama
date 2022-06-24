30e9fdc541e8ac63704219dd49b9c1969753588b
Matheus Bertoti <100535920+matheusbertoti@users.noreply.github.com>
2022-06-24 18:54:49 -0300

    resolvendo problema das tupplas

--- cod_anagrama.py
+++ cod_anagrama.py
@@ -1,31 +1,3 @@
-# # Codificando letras para numeros (ordem alfabetica)
-# a = 1
-# b = 2
-# c = 3
-# d = 4
-# e = 5
-# f = 6
-# g = 7
-# h = 8
-# i = 9
-# j = 10
-# k = 11
-# l = 12
-# m = 13
-# n = 14
-# o = 15
-# p = 16
-# q = 17
-# r = 18
-# s = 19
-# t = 20
-# u = 21
-# v = 22
-# w = 23
-# x = 24
-# y = 25
-# z = 26
-
 # Importanto as bibliotecas necessarias
 from itertools import permutations
 from unittest import skip
@@ -44,12 +16,12 @@ while ' ' in letras:
     letras.remove(' ')
 print(letras)
 
-# Fazendo leitura do dicionario 
-lista_de_palavras = open('dicionario_pt-br.txt', "r", encoding='utf-8')
-dicionario = []
-for line in lista_de_palavras:
-    dicionario.append(line.strip())
-# print(dicionario[4])
+# # Fazendo leitura do dicionario 
+# lista_de_palavras = open('dicionario_pt-br.txt', "r", encoding='utf-8')
+# dicionario = []
+# for line in lista_de_palavras:
+#     dicionario.append(line.strip())
+# # print(dicionario[4])
 
 
 # criando permutacoes da palavra digitada
